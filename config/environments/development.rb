3a1d9fda55a0d64d91b35987fd8e5fee7bb863f7
Dane Summers <dsummersl@yahoo.com>
2019-03-05 20:19:46 -0500

    Configure email to use sendgrid.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,10 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # For Mailcatcher
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {address: 'localhost', port: 1025}
 end
 
 ActionMailer::Base.smtp_settings = {
