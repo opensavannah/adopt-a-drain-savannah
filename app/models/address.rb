f65b19c6431031ce14f737e4efb2b61a825aff16
Robert Hammond <roberth05@gmail.com>
2019-08-04 10:23:51 -0400

    Fixes address lookup
    Resolves issue #3

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,8 @@
 class Address
   include Geokit::Geocoders
 
+  Geokit::Geocoders::GoogleGeocoder.api_key = ENV['GOOGLE_MAPS_KEY']
+
   def self.geocode(address)
     MultiGeocoder.geocode(address).ll.split(',').collect(&:to_f)
   end
