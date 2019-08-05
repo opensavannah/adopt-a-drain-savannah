4f8cfd266837ed3db59e2541e6b616ec4f6378ed
Robert Hammond <roberth05@gmail.com>
2019-08-05 06:29:59 -0400

    Revert "Made mail server variables generic"
    This reverts commit f5f5361f5385db0b25d17b861cc227cb4216377a.

--- config/environment.rb
+++ config/environment.rb
@@ -2,12 +2,12 @@
 require File.expand_path('../application', __FILE__)
 
 ActionMailer::Base.smtp_settings = {
-  address: ENV['MAILSERVER_HOST'],
+  address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
-  user_name: ENV['MAILSERVER_USERNAME'],
-  password: ENV['MAILSERVER_PASSWORD'],
-  domain: ENV['MAILSERVER_DOMAIN'],
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'heroku.com',
   enable_starttls_auto: true,
 }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,10 @@ Rails.application.configure do
 end
 
 ActionMailer::Base.smtp_settings = {
-  address: ENV['MAILSERVER_DOMAIN'],
+  address: 'smtp.sendgrid.net',
   port: '25',
   authentication: :plain,
-  user_name: ENV['MAILSERVER_USERNAME'],
-  password: ENV['MAILSERVER_PASSWORD'],
-  domain: ENV['MAILSERVER_DOMAIN'],
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: ENV['SENDGRID_DOMAIN'],
 }
