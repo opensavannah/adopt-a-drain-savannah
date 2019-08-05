e1ce3c6162c6eab73d43c37351fa52ccf31b15b0
Robert Hammond <roberth05@gmail.com>
2019-08-05 06:33:40 -0400

    Generic mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,10 @@ Rails.application.configure do
 end
 
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: '25',
+  address: ENV['MAILSERVER_HOST'],
+  port: '587',
   authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  domain: ENV['SENDGRID_DOMAIN'],
+  user_name: ENV['MAILSERVER_USERNAME'],
+  password: ENV['MAILSERVER_PASSWORD'],
+  domain: ENV['MAILSERVER_DOMAIN'],
 }
