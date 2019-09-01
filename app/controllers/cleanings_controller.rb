3136df54d317ee14f851528cb184f70b4d1bc5c7
Robert Hammond <roberth05@gmail.com>
2019-09-01 17:20:19 -0400

    Ability to create new cleaning record

--- app/controllers/cleanings_controller.rb
+++ app/controllers/cleanings_controller.rb
@@ -1,2 +1,21 @@
 class CleaningsController < ApplicationController
+    respond_to :html, :json
+    def create
+        @cleaning = Cleaning.new(cleaning_params)
+        if @cleaning.save
+            respond_to do |format|
+                format.html { redirect_to :root }
+                format.json { render(json: @cleaning) }
+            end
+        else
+            render(json: {errors: @thing.errors}, status: 500)
+        end        
+    end
+
+    private
+
+    def cleaning_params
+        params.require(:cleanings).permit(:date_cleaned, :diverted, :user_id, :thing_id)
+    end
+
 end
