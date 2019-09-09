9447360ebaed29455accacf521fb5bac7e337a17
LewisRa <rachel.lewis9312@gmail.com>
2019-09-09 18:50:26 -0400

    Revert "Update email message (#25)"
    This reverts commit 2cd3564ef3b11062cea0d02c0dd7c37a6f15fcf0.

--- test/mailers/devise_mailer_test.rb
+++ test/mailers/devise_mailer_test.rb
@@ -7,7 +7,7 @@ class DeviseMailerTest < ActionMailer::TestCase
     email = DeviseMailer.reset_password_instructions(@user, '1234').deliver_now
 
     assert_not ActionMailer::Base.deliveries.empty?
-    assert_equal ['Info@adopt-a-drain-savannah.org'], email.from
+    assert_equal ['no-reply@sfwater.org'], email.from
     assert_equal ['erik@example.com'], email.to
     assert_equal 'Adopt-a-drain Savannah reset password instructions', email.subject
   end
